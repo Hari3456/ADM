b52250995cb8721f568a28b3ca3927bbaec8ba83
Harish Rana <89992616+Hari3456@users.noreply.github.com>
2025-02-28 09:21:11 +0530

    Update web.py

--- web.py
+++ web.py
@@ -2,15 +2,11 @@ import streamlit as st
 import al
 
 def main():
-    st.title("Web Information & APK Uploader")
+    st.title("Android Malware Detection using ML")
     
-    st.header("What is the Web?")
     st.write(
-        "The web, also known as the World Wide Web (WWW), is a system of interlinked hypertext documents accessed via the Internet. "
-        "Users can view web pages using a web browser and navigate between them using hyperlinks. The web enables communication, commerce, entertainment, and information sharing globally."
+        "Give an APK file in input below."
     )
-    
-    st.header(al.bec())
     uploaded_file = st.file_uploader("Choose an APK file", type=["apk"])
     
     if uploaded_file is not None:
